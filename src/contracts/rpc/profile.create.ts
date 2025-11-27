58b9a4285193be5b7a71fc63edca444803f351b8
Alexander Kosarev <alexcdt0494@gmail.com>
2025-11-27 16:33:43 +0200

    v3.2.0

---
+++ src/contracts/rpc/profile.create.ts
@@ -0,0 +1,23 @@
+import { IBaseRpcContract } from "./rpc.interface";
+
+type ReqType = {
+    firstName: string;
+    lastName: string;
+    patronymic?: string;
+    userId: string;
+    email: string;
+    phone?: {
+        national: string,
+        countryCode: string,
+        isoCode: string,
+    };
+};
+
+type ResType = {
+    success: true;
+};
+
+export class ProfileCreateRpcContract extends IBaseRpcContract<ReqType, ResType> {
+    static readonly cmd = 'profile.create';
+    readonly cmd = ProfileCreateRpcContract.cmd;
+}
