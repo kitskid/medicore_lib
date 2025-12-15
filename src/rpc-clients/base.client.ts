c8985d713655044714f21ee6ad63182028afa419
alex.gt.sa <alex.gt.sa@gmail.com>
2025-12-15 16:52:14 +0200

    Refactor send method in RpcBaseClient for improved readability and formatting; apply Prettier for consistent code style

--- src/rpc-clients/base.client.ts
+++ src/rpc-clients/base.client.ts
@@ -6,13 +6,21 @@ export abstract class RpcBaseClient {
     abstract readonly serviceName: string;
     abstract readonly queue: string;
 
-    constructor(private client: ClientProxy) {}
+    constructor(private client: ClientProxy) { }
 
     async send<T extends IBaseRpcContract<any, any>>(
-        eventClass: { new (...args: any[]): T; cmd: string },
+        eventClass: { new(...args: any[]): T; cmd: string },
         data: T['data'],
         timeout = 5000,
     ): Promise<T['response']> {
-        return firstValueFrom(this.client.send<T['response']>({ cmd: eventClass.cmd }, data).pipe(tm(timeout)));
+        // prettier-ignore
+        return firstValueFrom(
+            this.client
+                .send<T['response']>(
+                    { cmd: eventClass.cmd },
+                    data,
+                )
+                .pipe(tm(timeout)),
+        );
     }
 }
